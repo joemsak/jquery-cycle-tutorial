e222a87348cce2a841d9c02b049b1c73eeed5eb4
Joe Sak <joe@neotericdesign.com>
2010-03-12 09:22:43 -0600

    Separated handwritten JS into external file

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#cycle_area').cycle({
+		fx: 'scrollHorz',
+		speed: 'fast',
+		prev: '#prev',
+		next: '#next',
+		easing: 'easeInOutCubic'				
+	});			
+		
+});
