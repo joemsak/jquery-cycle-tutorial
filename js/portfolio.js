db576d2a2243b62a61d881fbdc252af42c9fc77b
Joe Sak <joe@neotericdesign.com>
2010-04-08 10:35:33 -0500

    LPCC added, and styles all smoothed out, ready for blog

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
   $('#cycle_area').cycle({
 		fx: 'scrollHorz',
-		speed: 1000,
+		speed: 500,
 		timeout: 5500,
-		prev: '#prev',
-		next: '#next',
 		pager: '#pager',
 		pagerAnchorBuilder: buildThumbnails,
 		easing: 'easeInOutCubic',
